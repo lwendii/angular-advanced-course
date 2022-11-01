bc213985ade55ebadcca4d142304f488a16f0f11
unknown <wendyli8620lw@gmail.com>
2022-11-01 14:14:46 +0100

    added custom input component with styling

---
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'au-fa-input',
+  templateUrl: './au-fa-input.component.html',
+  styleUrls: ['./au-fa-input.component.css']
+})
+export class AuFaInputComponent implements OnInit {
+  @Input() icon:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get classes() {
+    const cssClasses = {
+    };
+
+    if(this.icon) {
+      cssClasses['fa-' + this.icon] = true;
+    }
+
+    return cssClasses;
+  }
+
+}
