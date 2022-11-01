1797234e6402b91407e6b33b9ff265ba5dbaf844
unknown <wendyli8620lw@gmail.com>
2022-11-01 15:05:35 +0100

    added directive, hostlistener and hostbinding

--- au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,16 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChild, Input, OnInit, AfterContentInit, HostBinding } from '@angular/core';
+import { InputRefDirective } from '../common/input-ref.directive';
 
 @Component({
   selector: 'au-fa-input',
   templateUrl: './au-fa-input.component.html',
   styleUrls: ['./au-fa-input.component.css']
 })
-export class AuFaInputComponent implements OnInit {
+export class AuFaInputComponent implements AfterContentInit {
   @Input() icon: string;
+  @ContentChild(InputRefDirective, {static: true}) input: InputRefDirective;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterContentInit(): void {
+    if(!this.input) {
+      console.error('the au input needs an input inside its content');
+    } 
+  }
+
+  @HostBinding('class.input-focus') 
+  get isInputFocus() {
+    return this.input ? this.input.focus : false;
   }
 
   get classes() {
