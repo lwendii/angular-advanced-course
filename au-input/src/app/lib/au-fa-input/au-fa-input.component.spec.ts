6c68fc74231fa2bdcf1e74da28eb3c405f367995
unknown <wendyli8620lw@gmail.com>
2022-11-03 22:22:59 +0100

    update angular 11

--- au-input/src/app/lib/au-fa-input/au-fa-input.component.spec.ts
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AuFaInputComponent } from './au-fa-input.component';
 
@@ -6,7 +6,7 @@ describe('AuFaInputComponent', () => {
   let component: AuFaInputComponent;
   let fixture: ComponentFixture<AuFaInputComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AuFaInputComponent ]
     })
